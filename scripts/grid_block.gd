1020421337d642628db4cd0403713de275cf9316
Neelesh Chevuri <neelesh.chevuri25@gmail.com>
2025-01-08 22:39:39 -0600

    Got grid to spawn based on parameters

--- scripts/grid_block.gd
+++ scripts/grid_block.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
+@export_category("Grid Block")
+@export var is_alive: bool = false
+@export var alive_color: Color = Color(1, 0, 0)
+@export var dead_color: Color = Color(.25, .25, .25)
+
+var parentSprite: Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	parentSprite = get_parent()
+	update_state()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +20,12 @@ func _process(delta: float) -> void:
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("left_click"):
-		print("clicked")
+		toggle_state()
+		
+func toggle_state() -> void:
+	is_alive = !is_alive
+	update_state()
+	
+func update_state() -> void:
+	var status_color: Color = alive_color if is_alive else dead_color
+	parentSprite.self_modulate = status_color
--- scripts/grid_spawner.gd
+++ scripts/grid_spawner.gd
@@ -1,9 +1,30 @@
-extends Node
+extends Node2D
 
+@export_category("Grid Spawner")
+@export var grid_size: int = 3
+@export_range(1, 10) var block_size: int = 10
+@export var padding_px: int = 10
+
+@onready var block_preload = preload("res://scenes/presets/grid_block.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var window_width: int = DisplayServer.screen_get_size().x
+	var grid_midpoint: int = (block_size * 10 + padding_px) * grid_size / 2.0
+	position.x = 480 - (grid_midpoint / 2.0)
+	position.y = position.x
+	
+	var block_pos: Vector2 = Vector2.ZERO
+	for x in grid_size:
+		block_pos.x = (x * ((block_size * 10) + padding_px))
+		for y in grid_size:
+			block_pos.y = (y * ((block_size * 10) + padding_px))
+			var block: Node = block_preload.instantiate()
+			block.position = block_pos
+			block.scale.x = block_size
+			block.scale.y = block_size
+			add_child(block)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
